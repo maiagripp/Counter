c3341b0d6eb37961343667b8660864d7c975a129
maiagripp <maiaandradec@gmail.com>
2021-07-06 19:05:58 -0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+import styles from './styles';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class Counter extends Component {
+  state = {
+    counter: 0,
+  };
+  render() {
+    return (
+      <View style={styles.view}>
+        <Text style={styles.text}> {this.state.counter} </Text>
+        <Button
+          title="+"
+          onPress={() => {
+            this.setState({ counter: this.state.counter + 1 });
+          }}
+        />
+
+        <Button
+          color="#a83232"
+          title="-"
+          onPress={() => {
+            this.setState({ counter: this.state.counter - 1 });
+          }}
+        />
+        <Button
+          color="#c3c3c3"
+          title="Reset"
+          onPress={() => {
+            this.setState({ counter: 0 });
+          }}
+        />
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+  return (
+    <View>
+      <Counter />
+    </View>
+  );
+};
+
+export default App;
---
+++ styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  view: {
+    marginTop: 250,
+    justifyContent: 'center',
+  },
+  buttonPlus: { backgroundColor: 'red' },
+  text: { textAlign: 'center', fontSize: 20 },
+});
+
+export default styles;
